0d106112e83e89cedcf53ebd10a23f7ed3bd20d1
ReivenIV <runyalen88@duck.com>
2025-01-10 17:46:29 +0200

    refs added

--- README.mk
+++ README.mk
@@ -1,2 +1,9 @@
 Simple recomendation. 
-Keep comands.txt open during the defence. Feel free to add update staff in it make it yours.. And remember.. VMs are pretty old school now a days we use docker and containers. 
+Keep comands.txt open during the defence. Feel free to add update staff in it make it yours.. And remember.. VMs are pretty old school now a days we use docker and containers.
+
+----
+
+Somme references i used : 
+- https://github.com/pasqualerossi/Born2BeRoot-Guide
+- https://github.com/mcombeau/Born2beroot
+- https://github.com/chlimous/42-born2beroot_guide?tab=readme-ov-file#setting-up-wordpress
